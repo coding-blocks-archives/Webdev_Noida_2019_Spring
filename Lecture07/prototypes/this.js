6dfa36c192b74184ca4597e9b63d124ed5731772
Arnav Gupta <arnav@codingblocks.com>
2019-02-17 11:17:15 +0530

    more about this
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture07/prototypes/this.js
+++ Lecture07/prototypes/this.js
@@ -6,13 +6,23 @@ let x = {
   t: tellThis,
   t2: function () {
     return tellThis()
+  },
+  t3: function () {
+    return this.t()
+  },
+  t4: function () {
+    return this.t2()
   }
 }
 
 function MyClass () {
 
 }
+
 MyClass.prototype.tt = tellThis
+MyClass.prototype.tt2 = function () {
+  return tellThis()
+}
 
 let obj = new MyClass()
 
