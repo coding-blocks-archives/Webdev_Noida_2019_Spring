c1cf009097d02bd11cec4d814d86440dd52cbabf
Arnav Gupta <arnav@codingblocks.com>
2019-02-17 10:48:40 +0530

    prototypes and constructor functions
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture07/prototypes/constructor-functions.js
@@ -0,0 +1,7 @@
+function Person (name, age) {
+  this.name = name
+  this.age = age
+}
+
+let p = new Person('Harry Potter', 20)
+let q = new Person('Ron Weasley', 20)
---
+++ Lecture07/prototypes/prorotypes.js
@@ -0,0 +1,12 @@
+let a = {
+  p: 10,
+  q: 20,
+  r: 'sfbs'
+}
+
+let b = Object.create(a)
+b.k = 'dgn'
+b.l = true
+b.m = 53
+
+let c = Object.create(b)
