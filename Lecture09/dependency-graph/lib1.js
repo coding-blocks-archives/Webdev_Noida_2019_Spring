c1b342b7bca410ab65335185e88873f51b85b13f
Arnav Gupta <arnav@codingblocks.com>
2019-03-03 11:47:18 +0530

    recursed require
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture09/dependency-graph/lib1.js
@@ -0,0 +1,6 @@
+const lib2 = require('./lib2')
+console.log('running lib1')
+module.exports = {
+  a: 10,
+  lib2
+}
---
+++ Lecture09/dependency-graph/lib2.js
@@ -0,0 +1,6 @@
+const lib1 = require('./lib1')
+console.log('running lib2')
+module.exports = {
+  b: 20,
+  lib1
+}
