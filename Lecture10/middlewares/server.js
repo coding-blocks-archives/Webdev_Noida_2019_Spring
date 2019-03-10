71320c5b5336f1841de708c184b4ba5fa6b528c3
Arnav Gupta <arnav@codingblocks.com>
2019-03-10 10:57:44 +0530

    middleware in get
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture10/middlewares/server.js
+++ Lecture10/middlewares/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-function m1 (req, res, next) {
+function m1(req, res, next) {
   console.log('running m1')
   if (req.query.m == 1) {
     return res.send('hello one')
@@ -9,22 +9,34 @@ function m1 (req, res, next) {
   next()
 }
 
-function m2 (req, res, next) {
+function m2(req, res, next) {
   console.log('running m2')
+  console.log(req.url)
   next()
 }
 
-function m3 (req, res, next) {
+function m3(req, res, next) {
   console.log('running m3')
   next()
 }
 
 app.use(m1)
 app.use(m2)
-app.use(m3)
+app.use('/bye', m3) // /bye, /bye/....
 
 app.get('/', (req, res) => {
   res.send('hello')
+})
+
+app.get('/bye', (req, res, next) => {
+  if (req.query.end == 1) {
+    return res.send('Goodbye')
+  }
+  next()
+})
+
+app.get('/bye', (req, res) => {
+  res.send('Farewell')
 })
 
 app.listen(5555)
