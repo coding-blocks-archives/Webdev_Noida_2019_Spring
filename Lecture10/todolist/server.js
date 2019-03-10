6a4ec79f4a1ea07c9fb984b13ff7d684174c4fae
Arnav Gupta <arnav@codingblocks.com>
2019-03-10 11:52:00 +0530

    Express todolist
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture10/todolist/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+let tasks = [] // {name: 'asdasd', priority: 3}
+
+app.get('/tasks', (req, res) => {
+  res.send(tasks)
+})
+
+app.post('/tasks', (req, res) => {
+  tasks.push({
+    name: req.body.name,
+    priority: +(req.body.priority || 1)
+  })
+  res.send({
+    taskId: tasks.length - 1,
+    success: true
+  })
+})
+
+app.listen(5454)
